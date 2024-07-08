3bc8b5c848027a530a81d7c8823db5035834e365
Racheal-Bato <rachealmwubaha8@gmail.com>
2024-07-08 10:13:58 +0300

    files added

---
+++ wildlife/index.js
@@ -0,0 +1,24 @@
+//CONDITIONAL STATEMENTS
+
+//if statements
+let age = 18;
+
+if(age>=18){
+    console.log('Eligible to get a national ID');
+}
+else if (age<18){
+    console.log('You are not eligible to get a national ID');
+    
+} 
+else{
+    console.log('Invalid age number');
+}
+//FUNCTIONS
+/*Syntax of a function
+function functionname(parameters){
+    statement;
+}*/
+
+
+
+
